71c37c101af46dcfbe14cdfdd0e364e4cbba6216
Christopher Dyken <cdyk@users.noreply.github.com>
2018-09-22 20:33:52 +0200

    Added insertion sort

--- src/sort.cpp
+++ src/sort.cpp
@@ -1,7 +1,22 @@
 #include <cstdio>
 #include <chrono>
 #include <vector>
+#include <cassert>
 #include <algorithm>
+
+template<typename T>
+__declspec(noinline) void insertionSort(T* data, size_t N, bool(*lessThan)(const T&, const T&))
+{
+  for (size_t j = 1; j < N; j++) {
+    auto t(std::move(data[j]));
+    size_t i = j;
+    for (; 0 < i && lessThan(data[i - 1], t); i--) {
+      data[i] = std::move(data[i - 1]);
+    }
+    data[i] = std::move(t);
+  }
+}
+
 
 template<typename T>
 __declspec(noinline) void stlSort(T* data, size_t N, bool (*lessThan)(const T&, const T&))
@@ -17,27 +32,15 @@ namespace {
     return a < b;
   }
 
-}
-
-
-int main(int argc, char** argv)
-{
-  std::srand(42);
   std::vector<unsigned> values;
+  std::vector<unsigned> gold;
   std::vector<std::vector<unsigned>> tmp;
 
-  unsigned its = 10;
-  tmp.resize(its);
 
-  size_t N = 1;
-  for (unsigned i = 0; i < 6; i++) {
-
-    auto a = values.size();
-    values.resize(N);
-    for (auto k = a; k < N; k++) {
-      values[k] = std::rand();
-    }
-
+  void runner(size_t N, const std::string& name, void(*impl)(unsigned* data, size_t N, bool(*)(const unsigned&, const unsigned&)))
+  {
+    unsigned its = 10;
+    tmp.resize(10);
     for (unsigned it = 0; it < its; it++) {
       tmp[it].resize(N);
       std::memcpy(tmp[it].data(), values.data(), sizeof(unsigned)*N);
@@ -48,7 +51,34 @@ int main(int argc, char** argv)
       stlSort<unsigned>(tmp[it].data(), N, lessThan);
     }
     auto stop = std::chrono::high_resolution_clock::now();
-    fprintf(stderr, "%5zu std::sort %10lldns\n", N , std::chrono::duration_cast<std::chrono::nanoseconds>((stop - start)).count()/its);
+    for (size_t i = 0; i < N; i++) {
+      assert(gold[i] == tmp.back()[i]);
+    }
+    fprintf(stderr, "%7zu %s %15lldns\n", N, name.c_str(), std::chrono::duration_cast<std::chrono::nanoseconds>((stop - start)).count() / its);
+  }
+
+}
+
+int main(int argc, char** argv)
+{
+  std::srand(42);
+
+  size_t N = 1;
+  for (unsigned i = 0; i < 9; i++) {
+
+    auto a = values.size();
+    values.resize(N);
+    for (auto k = a; k < N; k++) {
+      values[k] = std::rand();
+    }
+
+    gold.resize(N);
+    std::memcpy(gold.data(), values.data(), sizeof(unsigned)*N);
+    std::sort(gold.begin(), gold.end());
+
+    runner(N, "std::sort    ", stlSort);
+    runner(N, "insertionSort", insertionSort);
+
     N = 7 * N + N / 3;
   }
 
